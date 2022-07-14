7fbf587765b745474c3b2f494dabcfd63970fcef
류건열 <rjsduf0503@naver.com>
2022-07-14 17:50:03 +0900

    fix: 앱 로그 필터링 버그 픽스
    #34

--- lib/components/app_log/app_log_contents.dart
+++ lib/components/app_log/app_log_contents.dart
@@ -43,7 +43,7 @@ class AppLogContents extends StatelessWidget {
                 ],
               );
             },
-            itemCount: provider.filteredBuffer.length,
+            itemCount: provider.filteredBufferWithoutPrefix.length,
           ),
         ),
       ),
